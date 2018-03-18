d89005bd33f9cb0505dd2ee63be7a2bc08b7448c
Tallerik <tallerik3@gmail.com>
2018-03-18 15:47:39 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -4,3 +4,37 @@ $(function() {
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 100}, 500, 'linear');
   });
 });
+
+$(function() {
+
+  $(window).scroll(function() {
+    var win = $(window);
+    var scroll = win.scrollTop();
+
+    var ob1 = $('#1').scrollTop();
+    var ob2 = $('#2').scrollTop();
+    var ob3 = $('#3').scrollTop();
+    var ob4 = $('#4').scrollTop();
+    var ob5 = $('#5').scrollTop();
+    var ob6 = $('#6').scrollTop();
+
+
+    if(scroll >= ob1 && scroll < ob2) {
+      console.log('1')
+    } else if(scroll >= ob2 && scroll < ob3) {
+      console.log('2')
+    } else if(scroll >= ob3 && scroll < ob4) {
+      console.log('3')
+    } else if(scroll >= ob4 && scroll < ob5) {
+      console.log('4')
+    } else if(scroll >= ob5 && scroll < ob6) {
+      console.log('5')
+    } else if(scroll >= ob6) {
+      console.log('6')
+
+  }
+  console.log(ob3)
+  })
+  
+
+});
